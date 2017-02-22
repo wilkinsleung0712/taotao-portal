c20b866dc064f1ea515dbddd962ef4e06a808855
wilkinsleung0712 <wilkinsweiqiangliang@gmail.com>
2017-02-22 16:18:27 +1100

    fixed incorrect data type, from ItemParam to ItemParamItem, adding logic to return HTML snipps after looping through itemparamitem list

--- src/main/java/com/taotao/portal/service/impl/ItemServiceImpl.java
+++ src/main/java/com/taotao/portal/service/impl/ItemServiceImpl.java
@@ -1,12 +1,15 @@
 package com.taotao.portal.service.impl;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.taotao.pojo.TaotaoResult;
 import com.taotao.pojo.TbItemDesc;
-import com.taotao.pojo.TbItemParam;
+import com.taotao.pojo.TbItemParamItem;
 import com.taotao.portal.pojo.ItemInfo;
 import com.taotao.portal.service.ItemService;
 import com.taotao.util.HttpClientUtils;
@@ -77,13 +80,33 @@ public class ItemServiceImpl implements ItemService {
     public String getItemParamById(Long itemId) {
         try {
             // 向taotao-rest发出HTTP请求商品描述并得到一个json数据
-            String itemParamJson = HttpClientUtils.doGet(REST_BASE_URL + ITEM_PARAM_URL + itemId);
+            String itemParamItemJson = HttpClientUtils.doGet(REST_BASE_URL + ITEM_PARAM_URL + itemId);
             // json数据需要转化成TaotaoResult类型
             // 需要把Object类型转化成TbItemDesc类型
-            TaotaoResult result = TaotaoResult.formatToPojo(itemParamJson, TbItemParam.class);
+            TaotaoResult result = TaotaoResult.formatToPojo(itemParamItemJson, TbItemParamItem.class);
             if (result.getStatus() == 200) {
-                TbItemParam tbItemParam = (TbItemParam) result.getData();
-                return tbItemParam.getParamData();
+                TbItemParamItem tbItemParamItem = (TbItemParamItem) result.getData();
+                // 这是储存在数据库的Json格式的paramdata
+                String paramData = tbItemParamItem.getParamData();
+                // 转化成数据列表
+                List<Map> jsonList = JsonUtils.jsonToList(paramData, Map.class);
+                // 转化成HTML片段
+                StringBuffer  sb=new StringBuffer();
+                sb.append("<table cellpadding=\"1\"  cellspacing=\"1\"  width=\"100%\"  border=\"0\"  >\n");
+                for(Map m1:jsonList){
+                    sb.append("  <tr>\n");
+                    sb.append("<th colspan=\"2\">"+m1.get("group")+"</th>\n");
+                    sb.append( "</tr>\n");
+                    List<Map> list2=(List<Map>) m1.get("params");
+                    for (Map m2 : list2) {
+                        sb.append("  <tr>\n");
+                        sb.append("<th>"+m2.get("k")+"</th>\n");
+                        sb.append("<th>"+m2.get("v")+"</th>\n");
+                        sb.append( "</tr>\n");          }
+                }
+                
+                sb.append(" </table>");
+                return sb.toString();
             }
         } catch (Exception e) {
             // TODO Auto-generated catch block

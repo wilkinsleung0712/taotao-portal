85da1d0a273f13e8ef32c876c7bd296d37b84ef5
wilkinsleung0712 <wilkinsweiqiangliang@gmail.com>
2017-02-28 15:20:30 +1100

    adding comments and provide ajax call on user logout

--- src/main/webapp/js/taotao.js
+++ src/main/webapp/js/taotao.js
@@ -1,17 +1,20 @@
 var TT = TAOTAO = {
-	checkLogin : function(){
+	checkLogin : function() {
+		// 需要提取token信息，如果系统已经成功登陆，后台会把用户信息写在cookie里面
 		var _ticket = $.cookie("TT_TOKEN");
-		if(!_ticket){
-			return ;
+		if (!_ticket) {
+			return;
 		}
+		
 		$.ajax({
 			url : "http://localhost:8084/user/token/" + _ticket,
 			dataType : "jsonp",
 			type : "GET",
-			success : function(data){
-				if(data.status == 200){
+			success : function(data) {
+				if (data.status == 200) {
 					var username = data.data.username;
-					var html = username + "，欢迎来到淘淘！<a href=\"http://www.taotao.com/user/logout.html\" class=\"link-logout\">[退出]</a>";
+					var html = username
+							+ "，欢迎来到淘淘！<a href=\"javascript:void()\" class=\"link-logout\" onclick=\"logout()\">[退出]</a>";
 					$("#loginbar").html(html);
 				}
 			}
@@ -19,7 +22,24 @@ var TT = TAOTAO = {
 	}
 }
 
-$(function(){
+$(function() {
+	// Jquery方法会在完成DOM加载后被调用
 	// 查看是否已经登录，如果已经登录查询登录信息
 	TT.checkLogin();
 });
+
+function logout() {
+	// 需要提取token信息，如果系统已经成功登陆，后台会把用户信息写在cookie里面
+	var _ticket = $.cookie("TT_TOKEN");
+	// 提交logout请求
+	$.ajax({
+		url:"http://localhost:8084/user/logout/" + _ticket,
+		dataType : "jsonp",
+		type:"GET",
+		success:function(data){
+			if(data.status == 200){
+				location="http://localhost:8082/";
+			}
+		}
+	});
+}
